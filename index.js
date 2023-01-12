4699072779699c0da9cc21c8939a2d38750762b1
René Morgado <renemorgado33@gmail.com>
2023-01-12 16:08:40 -0600

    feature/changed-port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const images = require('./routes/images')
 const user = require('./routes/user')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
